d83845bc24ae3410b19b00c0dc9a83ef68df8c97
smu02 <sean.muir@sky.uk>
2026-04-01 14:40:33 +0100

    dir patch

--- teardown.sh
+++ teardown.sh
@@ -3,6 +3,8 @@ set -e
 
 podman rm -f zigbee-matter-manager 2>/dev/null || true
 
+export branch="main"
+
 image=$(podman images | grep "localhost/zigbee-matter-manager" | awk '{print $3}')
 
 if [[ -n "$image" ]]; then
@@ -11,10 +13,10 @@ else
     echo "No existing image found, skipping rmi."
 fi
 
-rm -rf zigbee-matter-manager/ .zigbee-matter-manager/
+rm -rf ~/zigbee-matter-manager/ ~/.zigbee-matter-manager/
 
 rm -f ~/.config/systemd/user/container-zigbee-matter-manager.service
 
 systemctl --user daemon-reload
 
-curl -fsSL https://raw.githubusercontent.com/oneofthemany/ZigBee-Matter-Manager/main/build.sh | bash -s -- --usb /dev/ttyUSB0
+curl -fsSL https://raw.githubusercontent.com/oneofthemany/ZigBee-Matter-Manager/${branch}/build.sh | bash -s -- --usb /dev/ttyUSB0
