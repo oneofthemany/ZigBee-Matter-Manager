44c39508ea6c858ff22ac53530b54cbc3f0b0f67
smu02 <sean.muir@sky.uk>
2026-08-30 16:10:49 +0100

    version increase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         // Android compares to decide an upgrade is an upgrade; versionName is
         // what the hero strip shows, and is the only way to tell from the phone
         // which build is actually running. CHANGELOG.md records what changed.
-        versionCode = 10
-        versionName = "1.6.0"
+        versionCode = 11
+        versionName = "1.7.0"
     }
 
     signingConfigs {
